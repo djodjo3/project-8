e5433d7b29c152fff53c5492b55b32e51e37bcdc
Claude-Arnaud PERROT <djodjo@MacBook-Pro-de-Claude-Arnaud.local>
2016-04-13 23:18:30 +0200

    Adding logs and stuff

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
-});
-
-
-server.start((err) => {
+server.register(require('inert'), (err) => {
 
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info.uri);
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*',
+		ops: '*'
+            }
+        }]
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+           throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
