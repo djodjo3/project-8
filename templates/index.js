b6b4304d8fd515ab924e562c9123d2ca7512297a
Claude-Arnaud PERROT <djodjo@MacBook-Pro-de-Claude-Arnaud.local>
2016-06-01 00:35:47 +0200

    Adding faviconnn \!

--- templates/index.js
+++ templates/index.js
@@ -6,6 +6,7 @@
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
+      <link href="/favicon.ico" rel="icon" type="image/x-icon" />
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
